844bc9b1d5e0a903f82ff3f3929887079bc62ace
James Warner <jmswrnr@users.noreply.github.com>
2020-02-16 23:43:09 +0000

    Test commit
    Does this work?

--- 3d-header/loading-3d-model/src/index.js
+++ 3d-header/loading-3d-model/src/index.js
@@ -1,7 +1,9 @@
 import "./styles.css";
 
+import * as THREE from "three";
+
 document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
+<h1>Test Commit!</h1>
 <div>
   We use Parcel to bundle this sandbox, you can find more info about Parcel
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
