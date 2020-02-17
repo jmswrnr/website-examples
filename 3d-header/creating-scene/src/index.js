000933c4c0e86107d0d31015c8489243d50b6c9f
jmswrnr <jmswrnr@users.noreply.github.com>
2020-02-17 12:27:58 +0000

    Remove layers and group

--- 3d-header/creating-scene/src/index.js
+++ 3d-header/creating-scene/src/index.js
@@ -1,17 +1,9 @@
 import './styles.css'
 import * as THREE from 'three'
 
-// Constants
-
-const DEFAULT_LAYER = 0
-const OCCLUSION_LAYER = 1
-
-// Create Scene, Group + Camera
+// Create Scene + Camera
 
 const mainScene = new THREE.Scene()
-
-const modelContainer = new THREE.Group()
-mainScene.add(modelContainer)
 
 const mainCamera = new THREE.PerspectiveCamera(
   20,
@@ -25,17 +17,14 @@ mainCamera.position.z = 10
 
 const backLight = new THREE.PointLight(0x00aaff, 3, 20)
 backLight.position.set(-5, 5, -5)
-backLight.layers.enable(OCCLUSION_LAYER)
 mainScene.add(backLight)
 
 const fillLight = new THREE.PointLight(0x00aaff, 0.7, 20)
 fillLight.position.set(-5, 0, 5)
-fillLight.layers.enable(OCCLUSION_LAYER)
 mainScene.add(fillLight)
 
 const keyLight = new THREE.PointLight(0xff00ff, 2, 20)
 keyLight.position.set(5, 0, 0)
-keyLight.layers.enable(OCCLUSION_LAYER)
 mainScene.add(keyLight)
 
 // Create Renderer
@@ -44,13 +33,6 @@ const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.getElementById('app').appendChild(renderer.domElement)
 
-function animate() {
-  modelContainer.rotation.x += 0.01
-  modelContainer.rotation.y += 0.01
-  requestAnimationFrame(animate)
-  renderer.render(mainScene, mainCamera)
-}
-animate()
 
 // Handle Window Resize
 
@@ -66,4 +48,14 @@ window.addEventListener('resize', resizeRenderer)
 var geometry = new THREE.BoxGeometry()
 var material = new THREE.MeshPhongMaterial({ color: 0xffffff })
 var cube = new THREE.Mesh(geometry, material)
-modelContainer.add(cube)
+mainScene.add(cube)
+
+// Render Scene
+
+function render() {
+  cube.rotation.x += 0.01
+  cube.rotation.y += 0.01
+  requestAnimationFrame(render)
+  renderer.render(mainScene, mainCamera)
+}
+render()
