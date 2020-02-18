38f530c54eedc9a2d058a8edd81040760e697fb0
James Warner <jmswrnr@users.noreply.github.com>
2020-02-18 03:28:13 +0000

    Refactor to match creating-scene code structure

--- 3d-header/loading-3d-model/src/index.js
+++ 3d-header/loading-3d-model/src/index.js
@@ -2,17 +2,10 @@ import './styles.css'
 import * as THREE from 'three'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
-// Constants
 
-const DEFAULT_LAYER = 0
-const OCCLUSION_LAYER = 1
-
-// Create Scene, Group + Camera
+// Create Scene + Camera
 
 const mainScene = new THREE.Scene()
-
-const modelContainer = new THREE.Group()
-mainScene.add(modelContainer)
 
 const mainCamera = new THREE.PerspectiveCamera(
   20,
@@ -26,17 +19,14 @@ mainCamera.position.z = 10
 
 const backLight = new THREE.PointLight(0x00aaff, 3, 20)
 backLight.position.set(-5, 5, -5)
-backLight.layers.enable(OCCLUSION_LAYER)
 mainScene.add(backLight)
 
 const fillLight = new THREE.PointLight(0x00aaff, 0.7, 20)
 fillLight.position.set(-5, 0, 5)
-fillLight.layers.enable(OCCLUSION_LAYER)
 mainScene.add(fillLight)
 
 const keyLight = new THREE.PointLight(0xff00ff, 2, 20)
 keyLight.position.set(5, 0, 0)
-keyLight.layers.enable(OCCLUSION_LAYER)
 mainScene.add(keyLight)
 
 // Create Renderer
@@ -44,14 +34,6 @@ mainScene.add(keyLight)
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.getElementById('app').appendChild(renderer.domElement)
-
-function animate() {
-  modelContainer.rotation.x += 0.01
-  modelContainer.rotation.y += 0.01
-  requestAnimationFrame(animate)
-  renderer.render(mainScene, mainCamera)
-}
-animate()
 
 // Handle Window Resize
 
@@ -67,27 +49,26 @@ window.addEventListener('resize', resizeRenderer)
 const loader = new GLTFLoader()
 const modelFile = require('../model/cybertruck.glb')
 
+const modelContainer = new THREE.Group()
+mainScene.add(modelContainer)
+
 loader.load(
   modelFile,
   gltf => {
-    // Add default mesh set to default layer
-    gltf.scene.layers.set(DEFAULT_LAYER)
     modelContainer.add(gltf.scene)
-
-    // Add black mesh set to occlusion Layer
-    const occlusionScene = gltf.scene.clone()
-    const blackMaterial = new THREE.MeshBasicMaterial({
-      color: new THREE.Color(0x000000),
-    })
-    occlusionScene.traverse(node => {
-      if (node instanceof THREE.Mesh) {
-        node = node.clone()
-        node.material = blackMaterial
-      }
-    })
-    occlusionScene.layers.set(OCCLUSION_LAYER)
-    modelContainer.add(occlusionScene)
   },
   undefined,
   console.error
 )
+
+// Render Scene
+
+function render() {
+  requestAnimationFrame(render)
+
+  modelContainer.rotation.x += 0.01
+  modelContainer.rotation.y += 0.01
+
+  renderer.render(mainScene, mainCamera)
+}
+render()
