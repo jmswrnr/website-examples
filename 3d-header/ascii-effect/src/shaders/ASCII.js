b68d2e986ffcf2447986ef1a8742c622deb19a3b
jmswrnr <jmswrnr@users.noreply.github.com>
2020-02-19 00:28:42 +0000

    ascii effect bug fixes + update

--- 3d-header/ascii-effect/src/shaders/ASCII.js
+++ 3d-header/ascii-effect/src/shaders/ASCII.js
@@ -5,11 +5,11 @@ export default () => ({
     tLowRes: { value: null },
     tFont: { value: null },
     tDepth: { value: null },
-    fontCharUv: { value: new THREE.Vector2(1, 1) },
+    fontCharTotalCount: { value: 0 },
+    fontCharCount: { value: new THREE.Vector2(1, 1) },
     fontCharSize: { value: new THREE.Vector2(1, 1) },
-    fontMapSize: { value: new THREE.Vector2(1, 1) },
+    renderCharCount: { value: new THREE.Vector2(1, 1) },
     renderCharSize: { value: new THREE.Vector2(1, 1) },
-    renderCharUv: { value: new THREE.Vector2(1, 1) },
     cameraNear: { value: 0 },
     cameraFar: { value: 1 },
   },
@@ -31,11 +31,12 @@ export default () => ({
     uniform float cameraNear;
     uniform float cameraFar;
 
-    uniform vec2 fontCharUv;
+    uniform float fontCharTotalCount;
     uniform vec2 fontCharSize;
-    uniform vec2 fontMapSize;
+    uniform vec2 fontCharCount;
+
+    uniform vec2 renderCharCount;
     uniform vec2 renderCharSize;
-    uniform vec2 renderCharUv;
 
     float readDepth(sampler2D depthSampler, vec2 coord) {
       float fragCoordZ = texture2D(depthSampler, coord).x;
@@ -45,23 +46,22 @@ export default () => ({
 
     void main()
     {
-      float depth = readDepth(tDepth, vUv);
-
       vec2 roundedUv = vec2(
-        floor(vUv.x * renderCharSize.x),
-        floor(vUv.y * renderCharSize.y)
-      ) * renderCharUv;
+        floor(vUv.x * renderCharCount.x),
+        floor(vUv.y * renderCharCount.y)
+      ) * renderCharSize;
 
+      float depth = readDepth(tDepth, roundedUv);
       vec4 color = texture2D(tLowRes, roundedUv);
     
-      float charIndex = (depth) * 80.0;
+      float charIndex = depth * fontCharTotalCount;
 
       vec2 fontuv = vec2(
-        mod(vUv.x, renderCharUv.x),
-        mod(vUv.y, renderCharUv.y)
-      ) * renderCharSize * fontCharUv + vec2(
-        floor(mod(charIndex, fontMapSize[0])) * fontCharUv[0],
-        floor(charIndex * fontCharSize[0]) * fontCharUv[0]
+        mod(vUv.x, renderCharSize.x),
+        mod(vUv.y, renderCharSize.y)
+      ) * renderCharCount * fontCharSize + vec2(
+        floor(mod(charIndex, fontCharCount.x)) * fontCharSize.x,
+        floor(charIndex * fontCharSize.y) * fontCharSize.y
       );
       
       gl_FragColor = texture2D(tFont, fontuv) * color;
